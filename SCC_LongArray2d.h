d13c144ddcb7ac0b528d7154abb22a1510385f64
Anderson <anderson@math.ucla.edu>
2020-07-16 08:17:15 -0700

    refactored to eliminate warnings

--- SCC_LongArray2d.h
+++ SCC_LongArray2d.h
@@ -355,7 +355,7 @@ friend std::ostream& operator<<(std::ostream& outStream, const LongArray2d&V )
 #endif
 
 #ifdef _DEBUG
-    bool sizeCheck(long size1, long size2, int coordinate)
+    bool sizeCheck(long size1, long size2, int /* coordinate */)
     {
     if(size1 != size2)
     {
@@ -365,7 +365,7 @@ friend std::ostream& operator<<(std::ostream& outStream, const LongArray2d&V )
     return true;
     }
 
-    bool sizeCheck(long size1, long size2, int coordinate) const
+    bool sizeCheck(long size1, long size2, int /* coordinate */) const
     {
     if(size1 != size2)
     {
